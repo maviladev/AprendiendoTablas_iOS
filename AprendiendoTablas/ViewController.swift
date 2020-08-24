f713fbc58e8cf023e7ff37770aeafec340f8e4e9
Marcos Avila <marcoa31@gmail.com>
2020-08-24 08:23:21 -0400

    uso de celdas predeterminadas

--- AprendiendoTablas/ViewController.swift
+++ AprendiendoTablas/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK; - Referencia de la tabla
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    /*
+     1.- Implementar DataSource <- interfaz
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //No olvidar
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "mi-celda")
+        //Para interactuar, implementar el delegate
+        tableView.delegate = self
     }
-
-
 }
 
+// Mark:
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("la celda #\(indexPath.row) fue seleccionada")
+    }
+}
+
+
+extension ViewController: UITableViewDataSource {
+    // 1. Número de filas que trendra nuestra tabla
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 20
+    }
+    
+    // 2. Metodo para saber que celdas deben mostrarse.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mi-celda", for: indexPath)
+        cell.textLabel?.text = "Soy la celda #\(indexPath.row)"
+        return cell
+    }
+}
